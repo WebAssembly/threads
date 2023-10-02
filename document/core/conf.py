bb1dd5892a1c425c490bb9405af9b8dd72accf4b
conrad <cnrdwtt@gmail.com>
2023-10-02 17:36:52 +0100

    add proposal name to top level

--- document/core/conf.py
+++ document/core/conf.py
@@ -69,7 +69,7 @@ logo = 'static/webassembly.png'
 repo = 'spec'
 
 # The name of the proposal it represents, if any
-proposal = ''
+proposal = 'Threads'
 
 # The draft version string (clear out for release cuts)
 draft = ' (Draft ' + date.today().strftime("%Y-%m-%d") + ')'
