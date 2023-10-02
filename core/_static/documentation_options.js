98147d2f991f75f3a897401710c0e4ad73d16cad
conrad <cnrdwtt@gmail.com>
2023-10-02 14:09:05 +0100

    publish: big updating appendices
    generated from commit cd12b7c4d0457a87bf387428b31d55e2d885687d

---
+++ core/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '2.0 (Draft 2023-10-02)',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
