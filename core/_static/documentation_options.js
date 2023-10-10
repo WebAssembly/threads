6bc2112206559ab310fa21fa2a8d92e4d7a690a4
conrad <cnrdwtt@gmail.com>
2023-10-10 01:03:43 +0100

    publish: add rule appendix entries, add initial wait/notify axioms
    generated from commit c04a679d885c83b58d6a8debda3a72109110c6af

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 + Threads (Draft 2023-10-07)',
+    VERSION: '2.0 + Threads (Draft 2023-10-10)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
